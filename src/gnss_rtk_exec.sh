464f894c07428fc41d7eba9ea3c1ac6f60211ec8
palia95 <56795288+palia95@users.noreply.github.com>
2024-07-16 14:45:18 +0200

    Added comment to BTS data

--- src/gnss_rtk_exec.sh
+++ src/gnss_rtk_exec.sh
@@ -3,7 +3,7 @@
 #Allow access to serial port
 sudo chmod a+rwx /dev/ttyACM0
 
-#Start RTK correction client from Ericsson base station
+#Start RTK correction client from Ericsson base station in Luleå (MCC; MNC and CI to select the base station)
 gnome-terminal -- bash -c "cd ~/catkin_ws/src/ublox_ros/SUPL-3GPP-LPP-client-main/build; ./example-lpp osr --ublox-port=uart2 --ublox-serial=/dev/ttyACM0 --host=lsp-dev.erdc.ericsson.net --port=5431 --mcc=240 --mnc=1 --ci=3000 --tac=1; exec bash"
 
 # Sleep for 10 seconds
